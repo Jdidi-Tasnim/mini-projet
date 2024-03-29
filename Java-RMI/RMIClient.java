4cdd3ee1f5001dbf9d1771aa46544d7ccbbc2ce5
Jdidi-Tasnim <jdiditasnim@gmail.com>
2024-03-29 01:01:17 +0100

    grpc update

--- Java-RMI/RMIClient.java
+++ Java-RMI/RMIClient.java
@@ -8,8 +8,18 @@ public class RMIClient {
             Registry registry = LocateRegistry.getRegistry("localhost", 1099);
             TaskManager taskManager = (TaskManager) registry.lookup("TaskManager");
 
-            // Use taskManager methods here
-            // Example: taskManager.addTask("Complete Project");
+            // Add a task
+            taskManager.addTask("Complete Project");
+
+            // Remove a task
+            taskManager.removeTask("Prepare Presentation");
+
+            // Get all tasks
+            List<String> allTasks = taskManager.getAllTasks();
+            System.out.println("All tasks:");
+            for (String task : allTasks) {
+                System.out.println(task);
+            }
         } catch (Exception e) {
             System.err.println("RMI Client exception: " + e.toString());
             e.printStackTrace();
